cadd6e00c40bed58c278ca722bb7b778b4f5337b
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-11-27 21:50:15 +0530

    Starting with about page

---
+++ stylestrom-app/src/components/About.tsx
@@ -0,0 +1,14 @@
+
+const About: React.FC = () => {
+  return (
+    <div id="about" className="min-h-screen w-screen">
+        <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+            <h2>
+                
+            </h2>
+        </div>
+    </div>
+  )
+}
+
+export default About
