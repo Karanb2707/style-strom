0341f5fd57684311cbf0c2c45ba66907c2d0001d
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-11-28 22:24:51 +0530

    Making heading and para in about section

--- stylestrom-app/src/components/About.tsx
+++ stylestrom-app/src/components/About.tsx
@@ -1,15 +1,52 @@
+import { useGSAP } from "@gsap/react";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+gsap.registerPlugin(ScrollTrigger);
+
 const About: React.FC = () => {
+  useGSAP(() => {
+    const clipAnimation = gsap.timeline({
+      scrollTrigger: {
+        trigger: "#clip",
+        start: "center center",
+        end: "+=800 center",
+        scrub: 0.5,
+        pin: true,
+        pinSpacing: true,
+      },
+    });
+
+    clipAnimation.to(".mask-clip-path", {
+      width: "100vw",
+      height: "100vh",
+      borderRadius: "0",
+    });
+  });
+
   return (
     <div id="about" className="min-h-screen w-screen">
-      <div className="relative mb-8 mt-36 flex flex-col items-center gap-5">
+      <div className="relative mb-8 mt-12 flex flex-col items-center gap-5">
         <h2 className="text-sm uppercase">
           Welcome to{" "}
-          <span className="text-2xl mx-1 font-semibold">GAMEHUB</span>
+          <span className="text-2xl mx-1 font-semibold">StyleStrom</span>
         </h2>
         <div className="mt-5 text-center text-4xl uppercase mx-10 tracking-tight md:text-[3rem]">
-          Discover the best largest shared style gaming platform
+          Unleash Your Style, Rewrite the Motion.
         </div>
-        <div></div>
+        <div className="">
+          <p>Where creativity flows into every frame you imagine.</p>
+          <p>A new wave of animation that moves with your vibe.</p>
+        </div>
+      </div>
+
+      <div className="h-dvh w-screen" id="clip">
+        <div className="mask-clip-path about-image">
+          <img
+            src="img/about.webp"
+            alt="Background"
+            className="absolute left-0 top-0 size-full object-cover"
+          />
+        </div>
       </div>
     </div>
   );
