c9d050f2bb1404ab593e3d8368475f229548264c
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-12-01 23:55:07 +0530

    Started feature section

--- stylestrom-app/src/components/Features.tsx
+++ stylestrom-app/src/components/Features.tsx
@@ -1,10 +1,21 @@
-
 const Features = () => {
   return (
-    <div>
-        
-    </div>
-  )
-}
+    <section className="bg-black pb-52">
+      <div className="container mx-auto px-3 md:px-10">
+        <div className="px-5 py-32 ">
+          <p className="text-lg text-blue-50">
+            Elevate Your Style. Define Your Identity.
+          </p>
+          <p className="max-w-d text-lg text-blue-50 opacity-50">
+            Step into a world where fashion meets expression. StyleStrom
+            empowers you to explore bold trends, unique looks, and endless
+            creativity. Redefine your wardrobe — and yourself — one style at a
+            time.
+          </p>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-export default Features
+export default Features;
