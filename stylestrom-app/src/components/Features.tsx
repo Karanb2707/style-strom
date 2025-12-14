ca0405343346a9602a5ccf93333df12125f5d4b9
Karanb2707 <95834281+Karanb2707@users.noreply.github.com>
2025-12-14 20:54:28 +0530

    added function handleMouseMove

--- stylestrom-app/src/components/Features.tsx
+++ stylestrom-app/src/components/Features.tsx
@@ -1,8 +1,43 @@
 import BentoCard from "./BentoCard";
 import type { BentoTiltProps } from "../types";
+import React, { useRef, useState } from "react";
 
 const BentoTilt = ({ children, className = "" }: BentoTiltProps) => {
-  return <div className={className}>{children}</div>;
+  const [transformStyle, setTransformStyle] = useState<string>("");
+  const itemRef = useRef<HTMLDivElement | null>(null);
+
+  const handleMouseMove = (e: React.MouseEvent<HTMLDivElement>) => {
+    if (!itemRef.current) return;
+
+    const { left, top, width, height } =
+      itemRef.current.getBoundingClientRect();
+
+    const relativeX = (e.clientX - left) / width;
+    const relativeY = (e.clientY - top) / height;
+
+    const tiltx = (relativeY - 0.5) * 5;
+    const tiltY = (relativeX - 0.5) * 5;
+
+    const newTransform = `perspective(700px) rotateX(${tiltx}deg) rotateY(${tiltY}deg) scale3d(0.98, 0.98, 0.98)`;
+
+    setTransformStyle(newTransform);
+  };
+
+  const handleMouseLeave = (e: React.MouseEvent<HTMLDivElement>) => {
+    setTransformStyle("");
+  };
+
+  return (
+    <div
+      className={className}
+      ref={itemRef}
+      onMouseMove={handleMouseMove}
+      onMouseLeave={handleMouseLeave}
+      style={{ transform: transformStyle }}
+    >
+      {children}
+    </div>
+  );
 };
 
 const Features = () => {
